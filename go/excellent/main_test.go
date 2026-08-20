8f580676d39d783c9f014e22f45dbffdf378c36a
unknown <yoshinori-watanabe@x-power.co.jp>
2026-08-20 17:13:05 +0900

    chg

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -5,7 +5,7 @@ import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
-	if resut != "even" {
+	if result != "even" {
 		t.Errorf("expected: even, actual: %s", result)
 	}
 }
